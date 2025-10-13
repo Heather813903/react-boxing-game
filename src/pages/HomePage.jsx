2da54f94754f300f24dbb1250090559dacbb7b11
Heather813903 <heathersmith9378@gmail.com>
2025-10-13 14:50:12 -0400

    feat: add Start Game button  on Homepage with navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,36 @@
+import { useNavigate } from "react-router-dom";
+
+
 function HomePage() {
+    const navigate = useNavigate();
+
+    const handleStart = () => {
+        navigate("/game");
+};
+
+
     return (
         <div style={{ textAlign: "center", marginTop: "2rem" }}>
             <h2>Welcome to React Boxing!</h2>
             <p>Click the button below to throw some punches!</p>
+            <button     
+                onClick={handleStart}
+                style={{ 
+                    backgroundColor: '#28a745',
+                    color: 'white',
+                    border: 'none',
+                    padding: '.0075rem 1.5rem',
+                    borderRadius: '10px',
+                    cursor: 'pointer',
+                    fontSize: '1rem',
+                    marginTop: '1rem',
+                }}
+                onMouseOver={(e) => (e.target.style.backgroundColor = '#218838')}
+                onMouseOut={(e) => (e.target.style.backgroundColor = '#28a745')}
+            >
+                Start Boxing!
+            </button>
+
         </div>
     );
 }
